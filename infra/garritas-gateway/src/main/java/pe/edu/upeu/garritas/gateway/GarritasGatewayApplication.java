e226a823c11dd35030e232d9e74b266c79564a6e
DeissyQuispeTorres <torresdeissy56@gmail.com>
2026-09-10 22:23:25 -0500

    Infraestructura completa: Gateway agregado

---
+++ infra/garritas-gateway/src/main/java/pe/edu/upeu/garritas/gateway/GarritasGatewayApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.garritas.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarritasGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarritasGatewayApplication.class, args);
+	}
+
+}
---
+++ infra/garritas-gateway/src/test/java/pe/edu/upeu/garritas/gateway/GarritasGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.garritas.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarritasGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
